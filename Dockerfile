78350ac324bb2d233d9524bcfc481389129513ef
Nishant Desai <nishant.desai@mail.utoronto.ca>
2019-08-10 09:59:45 -0400

    boiler plate app

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,12 @@ FROM node:12-slim
 
 WORKDIR /usr/src/app
 
-COPY package.json /usr/src/app
+COPY package*.json /usr/src/app
 
 RUN npm install
 
 COPY . /usr/src/app
+
+EXPOSE 3000
 
 CMD ["npm", "start"]
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    'mongodb://mongo:27017/docker-node-mongo',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+const port = 3000;
+
+app.listen(port, () => console.log('Server running...'));

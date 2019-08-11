577a1cf8270e2e05eae0bf8a0d42bd4e6e8b391f
Nishant Desai <nishant.desai@mail.utoronto.ca>
2019-08-11 10:28:17 -0400

    added template app

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Connect to MongoDB
 mongoose
   .connect(
-    'mongodb://mongo:27017/cbot',
+    'mongodb://mongo:27017/docker-node-mongo',
     { useNewUrlParser: true }
   )
   .then(() => console.log('MongoDB Connected'))
